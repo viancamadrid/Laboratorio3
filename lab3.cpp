9269d55201eaff532be7ccd312dce7d2bfb3ef95
viancamadrid <viancamadrid@unitec.edu>
2016-02-05 16:59:41 -0600

    llenar tabla con  random

--- lab3.cpp
+++ lab3.cpp
@@ -7,6 +7,7 @@ using std::endl;
 
 void ordenar(int []);
 void imprimir(int []);
+int datos(int, int, int [][]);
 
 int main( int argc, char*argv[]){
 	int opcion=0;
@@ -46,7 +47,51 @@ int main( int argc, char*argv[]){
 		imprimir(scores);	
 	}
 	if(opcion==2){
-
+		int matriz[4][3];
+		int num, num2;
+		for(int i=0; i<4; i++){
+			int num=85+rand()%((115+1)-85);
+			matriz[i][0]=num;
+		}
+		for(int i=0; i<4; i++){
+                        int num=50+rand()%((75+1)-50);
+                        matriz[i][1]=num;
+                } 
+		for(int i=0; i<4; i++){
+                        int num=150+rand()%((150+1)-200);
+                        matriz[i][2]=num;
+                }
+		int num, num1, num2, num4, num5, num6;
+		num=rand()%4+1;
+        	num2=rand()%4+1;
+		num3=rand()%4+1;
+		num4=rand()%4+1;
+        	num5=rand()%4+1;
+		num6=rand()%4+1;
+		int a1,a2,d1,d2,v1,v2;
+		a1=datos(num,0,matriz);
+		a2=datos(num,0,matriz);
+		d1=datos(num,1,matriz);
+		d2=datos(num,1,matriz);
+		v1=datos(num,2,matriz);
+		v2=datos(num,2,matriz);
+		int puntos1, puntos2; 
+		puntos1=a1-d2;
+		puntos2=a2-d1;
+		//int puntos1, puntso2;
+		//puntos=matriz[num][0];
+		if(puntos1=puntos2){
+			if(v1>v2)
+				cout<<"Jugador 1 Gano Por Velocidad"<<endl;
+			else
+				cout<<"Jugador 2 Gano Por Velocidad"<<endl;
+		}else{
+			if(puntos1>puntos2)
+				cout<<"Jugador 1 Gano con "<<puntos1<<" Puntos de Diferencia, No Gano Por Velocidad"<<endl;
+			else
+				
+			
+		}
 	}
 	return 0;
 }
@@ -68,4 +113,9 @@ void imprimir(int scores[]){
 	for(int i=0; i<10; i++){
 		cout<<"Posicion "<<i+1<<": "<<scores[i]<<endl; 
 	}
+}
+
+int datos(int i, int j, int matriz[][]){
+	int d= matriz[i][j];
+	return d;
 }
