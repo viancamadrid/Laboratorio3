c5467db32f6ec68749830ab2d631f96d1912595d
viancamadrid <viancamadrid@unitec.edu>
2016-02-05 17:51:23 -0600

    ejecicio 2 terminado

--- lab3.cpp
+++ lab3.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
 #include <cstdlib>
+#include <iomanip>
 
 using std::cout;
 using std::cin;
 using std::endl;
+using std::setw;
 
 void ordenar(int []);
 void imprimir(int []);
-int datos(int, int, int [][]);
+int datos(int, int, int [4][3]);
 
 int main( int argc, char*argv[]){
 	int opcion=0;
@@ -24,7 +26,9 @@ int main( int argc, char*argv[]){
 		//int temp=0;
 		cout<<"-----------"<<num<<"--------------"<<endl;
 		while(seguir==0){	
-			//int num=-500+rand()%((500+1)-(-500));
+			intentos++;
+                        cout<<"Ingrese Numero: ";
+                        cin>>adivinar;
 			while(adivinar!=num){
 				intentos++;
 				cout<<"Ingrese Numero: ";
@@ -47,6 +51,7 @@ int main( int argc, char*argv[]){
 		imprimir(scores);	
 	}
 	if(opcion==2){
+		srand(time(NULL));
 		int matriz[4][3];
 		int num, num2;
 		for(int i=0; i<4; i++){
@@ -61,36 +66,51 @@ int main( int argc, char*argv[]){
                         int num=150+rand()%((150+1)-200);
                         matriz[i][2]=num;
                 }
-		int num, num1, num2, num4, num5, num6;
-		num=rand()%4+1;
-        	num2=rand()%4+1;
-		num3=rand()%4+1;
-		num4=rand()%4+1;
-        	num5=rand()%4+1;
-		num6=rand()%4+1;
-		int a1,a2,d1,d2,v1,v2;
-		a1=datos(num,0,matriz);
-		a2=datos(num,0,matriz);
-		d1=datos(num,1,matriz);
-		d2=datos(num,1,matriz);
-		v1=datos(num,2,matriz);
-		v2=datos(num,2,matriz);
-		int puntos1, puntos2; 
-		puntos1=a1-d2;
-		puntos2=a2-d1;
-		//int puntos1, puntso2;
-		//puntos=matriz[num][0];
-		if(puntos1=puntos2){
+		for(int i=0; i<4; i++){
+			for(int j=0; j<3; j++){
+				cout<<setw(6)<<matriz[i][j]<<setw(6);
+			}
+			cout<<endl;
+		}
+
+		int n1, n2, n3, n4, n5, n6;
+		n1=rand()%4+1;
+		n2=rand()%4+1;
+		n3=rand()%4+1;
+		n4=rand()%4+1;
+		n5=rand()%4+1;
+		n6=rand()%4+1;
+	
+		//cout<<n1<<" "<<n2<<" "<<n3<<" "<<n4<<"  "<<n5<<"  "<<n6<<endl;
+		
+		int a1, a2, d1, d2, v1, v2;
+		a1=datos(n1-1,0,matriz);
+		a2=datos(n2-1,0,matriz);
+		d1=datos(n3-1,1,matriz);
+		d2=datos(n4-1,1,matriz);
+		v1=datos(n5-1,2,matriz);
+		v2=datos(n6-1,2,matriz);
+		
+		//cout<<a1<<" "<<a2<<" "<<d1<<" "<<d2<<"  "<<v1<<"  "<<v2<<endl;
+
+		int p1,p2;
+		p1=a1-d2;
+		p2=a2-d1;
+
+		cout<<"Juagador 1: ataque "<<a1<<", defensa "<<d1<<", velocidad "<<v1<<endl;
+		cout<<"Juagador 2: ataque "<<a2<<", defensa "<<d2<<", velocidad "<<v2<<endl;
+		
+		if(p1==p2){
 			if(v1>v2)
-				cout<<"Jugador 1 Gano Por Velocidad"<<endl;
+				cout<<"Jugador 1 gano por velocidad"<<endl;
 			else
-				cout<<"Jugador 2 Gano Por Velocidad"<<endl;
+				cout<<"Jugador 2 gano por velocidad"<<endl;
 		}else{
-			if(puntos1>puntos2)
-				cout<<"Jugador 1 Gano con "<<puntos1<<" Puntos de Diferencia, No Gano Por Velocidad"<<endl;
+			if(p1>p2)
+				cout<<"Jugador 1 gano con "<<p1-p2<<" puntos de diferencia, no gano por velocidad"<<endl;
 			else
-				
-			
+				cout<<"Jugador 2 gano con "<<p2-p1<<" puntos de diferencia, no gano por velocidad"<<endl;
+
 		}
 	}
 	return 0;
@@ -115,7 +135,10 @@ void imprimir(int scores[]){
 	}
 }
 
-int datos(int i, int j, int matriz[][]){
+int datos(int i, int j, int matriz[4][3]){
 	int d= matriz[i][j];
 	return d;
 }
+
+
+
